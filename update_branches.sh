63fecf1ff9f6a0adc6a41ba79f9b04ce11970150
Kuntal Majumder <kuntal@infracloud.io>
2021-01-07 12:34:30 +0530

    f

--- update_branches.sh
+++ update_branches.sh
@@ -7,10 +7,12 @@ for branch in $BRANCHES; do
     git checkout $branch
     git pull upstream $branch --rebase
     git push origin $branch
+    echo "Updated $branch"
 done
 
 for branch in $BRANCHES; do
     git checkout -b tetrate-$branch
     git rebase $branch
     git push origin tetrate-$branch
+    echo "Update tetrate-$branch"
 done

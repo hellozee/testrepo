0ec45cc123b1e632adf303b368679967dc714ad7
Kuntal Majumder <kuntal@infracloud.io>
2021-01-07 12:28:04 +0530

    t

--- update_branches.sh
+++ update_branches.sh
@@ -3,8 +3,6 @@
 UPSTREAM_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/istio/istio.git"
 BRANCHES=$(git branch -r | grep -E "origin/release-[0-9]+.[0-9]+$" | cut -d "/" -f 2)
 
-git remote add upstream $UPSTREAM_REPO
-
 for branch in $BRANCHES; do
     git checkout $branch
     git pull upstream $branch --rebase

75cdd4de787e8ac9cb2428e59c698219990dd0d6
Kuntal Majumder <kuntal@infracloud.io>
2021-01-07 14:01:19 +0530

    l

--- update_branches.sh
+++ update_branches.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 UPSTREAM_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/istio/istio.git"
-BRANCHES=$(git branch -r | grep -E "release-[0-9]+.[0-9]+$" | cut -d "/" -f 2)
+BRANCHES=$(git branch -r | grep -E "release-[0-9]+.[0-9]+$" | cut -d "/" -f 2 | tail -n +14)
+
+[[ ! $(git config remote.upstream.url) ]] && git remote add upstream $UPSTREAM_REPO
 
 echo $BRANCHES
 
@@ -15,6 +17,7 @@ done
 for branch in $BRANCHES; do
     git checkout -b tetrate-$branch
     git rebase $branch
+    git rebase hellozee
     git push origin tetrate-$branch
     echo "Update tetrate-$branch"
 done
